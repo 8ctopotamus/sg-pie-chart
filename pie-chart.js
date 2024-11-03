4cf72ace27ee33ceb0a35e9fe701fb208975c703
Josh <zylo.codes@gmail.com>
2024-11-03 08:25:36 -0600

    sets up d3

---
+++ pie-chart.js
@@ -0,0 +1,7 @@
+let width = 450, 
+    height = 450,
+    margin = 40
+
+let radius = Math.min(width, height) / 2 - margin
+
+let svg = d3.select('')
---
+++ sg-pie-chart.php
@@ -0,0 +1,14 @@
+<?php 
+
+/**
+  * Plugin Name: SG Pie Chart
+  * Plugin URI: https://github.com/8ctopotamus/sg-pie-chart
+  * Description: Animated pie chart
+  * Version: 1.0
+  * Author: Zylo, LLC
+  * Author URI: https://zylocod.es
+  * License: GPL2
+  */
+
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
