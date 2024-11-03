884e5fae732537683198f4651149dd2eea68020b
8ctopotamus <zylo.codes@gmail.com>
2024-11-03 09:11:37 -0600

    6 slices

--- pie-chart.js
+++ pie-chart.js
@@ -11,12 +11,16 @@ const svg = d3.select('#sg-pie-chart')
   .append('g')
     .attr('transform', `translate(${width / 2}, ${height / 2})`)
 
+const numSlices = 6
+const portion = 100 / numSlices 
+
 const data = {
-  a: 9,
-  b: 20,
-  c: 30,
-  d: 8,
-  e: 12,
+  a: portion,
+  b: portion,
+  c: portion,
+  d: portion,
+  e: portion,
+  f: portion
 }
 
 const color = d3.scaleOrdinal()
--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
 https://d3-graph-gallery.com/pie.html
+
+https://d3-graph-gallery.com/graph/pie_annotation.html
